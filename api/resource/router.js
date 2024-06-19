bdf87afaae46db3b40f6999c89f45c709057a278
Brian Howell <brianhowell@pm.me>
2024-06-18 21:29:23 -0400

    Implement Resources model and router

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,11 +1,22 @@
 // build your `/api/resources` router here
-
 const express = require("express");
 const Resources = require("./model");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.json("get resources");
+  Resources.getResources()
+    .then((resources) => {
+      res.json(resources);
+    })
+    .catch(next);
+});
+
+router.post("/", (req, res, next) => {
+  Resources.addResource(req.body)
+    .then((resource) => {
+      res.status(201).json(resource);
+    })
+    .catch(next);
 });
 
 module.exports = router;
