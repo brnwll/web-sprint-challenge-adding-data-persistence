d8b2866d1ff767a55ec1a8e36fdf057972f051c9
Brian Howell <brianhowell@pm.me>
2024-06-18 21:40:28 -0400

    Implement Tasks model and router

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,29 @@
 // build your `Task` model here
+
+const db = require("../../data/dbConfig");
+
+function getTasks() {
+  return db("tasks as t")
+    .leftJoin("projects as p", "t.project_id", "p.project_id")
+    .select(
+      "t.task_id",
+      "t.task_description",
+      "t.task_notes",
+      "t.task_completed",
+      "p.project_name",
+      "p.project_description"
+    );
+}
+
+function addTask(task) {
+  return db("tasks")
+    .insert(task)
+    .then(([task_id]) => {
+      return db("tasks").where("task_id", task_id).first();
+    });
+}
+
+module.exports = {
+  getTasks,
+  addTask,
+};
--- api/task/router.js
+++ api/task/router.js
@@ -5,7 +5,23 @@ const Tasks = require("./model");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.json("get tasks");
+  Tasks.getTasks()
+    .then((tasks) => {
+      tasks.forEach((task) => {
+        task.task_completed = !!task.task_completed;
+      });
+      res.json(tasks);
+    })
+    .catch(next);
+});
+
+router.post("/", (req, res, next) => {
+  Tasks.addTask(req.body)
+    .then((task) => {
+      task.task_completed = !!task.task_completed;
+      res.status(201).json(task);
+    })
+    .catch(next);
 });
 
 module.exports = router;
