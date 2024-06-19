8a8a665b4e9c7d377c64cd3c54562d78780ffcfd
Brian Howell <brianhowell@pm.me>
2024-06-18 21:37:40 -0400

    Implement Projects model and router

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,33 @@
 // build your `Project` model here
+
+/*
+
+
+- [ ] `[POST] /api/projects`
+  - Even though `project_completed` is stored as an integer, the API uses booleans when interacting with the client
+  - Example of response body: `{"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}`
+
+- [ ] `[GET] /api/projects`
+  - Even though `project_completed` is stored as an integer, the API uses booleans when interacting with the client
+  - Example of response body: `[{"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}]`
+
+  */
+
+const db = require("../../data/dbConfig");
+
+function getProjects() {
+  return db("projects");
+}
+
+function addProject(project) {
+  return db("projects")
+    .insert(project)
+    .then(([project_id]) => {
+      return db("projects").where({ project_id }).first();
+    });
+}
+
+module.exports = {
+  getProjects,
+  addProject,
+};
--- api/project/router.js
+++ api/project/router.js
@@ -5,7 +5,23 @@ const Projects = require("./model");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.json("get projects");
+  Projects.getProjects()
+    .then((projects) => {
+      projects.forEach((project) => {
+        project.project_completed = !!project.project_completed;
+      });
+      res.json(projects);
+    })
+    .catch(next);
+});
+
+router.post("/", (req, res, next) => {
+  Projects.addProject(req.body)
+    .then((project) => {
+      project.project_completed = !!project.project_completed;
+      res.status(201).json(project);
+    })
+    .catch(next);
 });
 
 module.exports = router;
